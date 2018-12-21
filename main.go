207ca51eb7b7fed2a0a585fc5e110eb3d363dc70
lmyaoi <renzo.russel@gmail.com>
2018-12-21 19:48:30 +0100

    removed small test

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os/exec"
 	"strconv"
 	"sync"
-	"time"
 	"vsync/flags"
 	"vsync/host"
 	"vsync/local"
@@ -18,12 +17,6 @@ import (
 )
 
 func main() {
-	t := time.Unix(5, 8000)
-	b, _ := t.MarshalJSON()
-	fmt.Printf("%q\n", b)
-	return
-
-
 	flag.Parse()
 	wg := &sync.WaitGroup{}
 
