8f793faf3982a1a931c035ac0f7106fa596bafb6
lmyaoi <renzo.russel@gmail.com>
2019-01-03 09:43:26 -0400

    fixed a bug which prevented multiple instances of vlc being opened

--- net/playback/vlc/run_darwin.go
+++ net/playback/vlc/run_darwin.go
@@ -8,7 +8,7 @@ import (
 
 func run() {
 	port := strconv.Itoa(conf.Get().VlcPort)
-	args := []string{"-a", "vlc", "--args", "--extraintf", "http", "--http-port", port, "--http-password", "q"}
+	args := []string{"-na", "vlc", "--args", "--extraintf", "http", "--http-port", port, "--http-password", "q"}
 	cmd := exec.Command("open", args...)
 	if err := cmd.Start(); err != nil {
 		panic(err)
